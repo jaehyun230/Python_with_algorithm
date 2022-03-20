fe05885d50ead74354516eabbd7592038447f87c
Jaehyun Park / 박재현 <48269869+jaehyun230@users.noreply.github.com>
2022-03-20 11:16:36 +0900

    Update DFS_example.py

--- DFS/DFS_example.py
+++ DFS/DFS_example.py
@@ -5,11 +5,11 @@ def dfs(graph, v, visited) :
   #현재 노드와 연결된 다른 노드를 재귀적으로 방문
   for i in graph[v]:
     if not visited[i]:
-      dfs(graph, i, visitied)
+      dfs(graph, i, visited)
       
 #각 노드 연결된 정보 (2차원 리스트)      
 graph = [
-  []
+  [],
   [2, 3, 8],
   [1, 7],
   [1, 4, 5],
@@ -21,7 +21,7 @@ graph = [
 ]
 
 #각노드 방문정보 그래프 초기화
-visitied = [False] * 9
+visited = [False] * 9
 
 #dfs 호출
 dfs(graph, 1, visited)
