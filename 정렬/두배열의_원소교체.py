428f0d4aa9940763b33c6d16d0b785baf95662d1
Jaehyun Park / 박재현 <48269869+jaehyun230@users.noreply.github.com>
2022-03-29 17:08:00 +0900

    Create 두배열의_원소교체.py

---
+++ 정렬/두배열의_원소교체.py
@@ -0,0 +1,17 @@
+n, k = map(int, input().split())
+
+array_A = list(map(int, input().split()))
+array_B = list(map(int, input().split()))
+
+array_A.sort()
+array_B.sort(reverse=True)
+for i in range (k) :
+  if array_A[i] < array_B[i] :
+    array_A[i] , array_B[i] = array_B[i] , array_A[i]
+  else :
+    break
+  if i == n-1 :
+    break
+
+print(sum(array_A))
+  
